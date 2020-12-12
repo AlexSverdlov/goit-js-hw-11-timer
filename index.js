b6e86b74cc069e912d1e5c144aa52a246dd20168
Alex Sverdlov <sverdlov.alexandr@gmail.com>
2020-12-12 19:09:00 +0200

    Finish

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-// Контрольная дата
-const targetDate1 = new Date(2021, 0);
+// Создаем класс CountdownTimer
 
-// Ссылки на элементы HTML
-const refs = {
-  days: document.querySelector('span[data-value=days]'),
-  hours: document.querySelector('span[data-value=hours]'),
-  mins: document.querySelector('span[data-value=mins]'),
-  secs: document.querySelector('span[data-value=secs]'),
-};
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  showTime() {
+    const targetDate2 = new Date();
+    const time = this.targetDate - targetDate2;
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-// Служебная функция
-function pad(value) {
-  return String(value).padStart(2, '0');
+    const refs = {
+      days: document.querySelector('span[data-value=days]'),
+      hours: document.querySelector('span[data-value=hours]'),
+      mins: document.querySelector('span[data-value=mins]'),
+      secs: document.querySelector('span[data-value=secs]'),
+    };
+
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+
+  tickTime() {
+    setInterval(() => this.showTime(), 1000);
+  }
 }
 
-// Функция считает элементы таймера и выводит на страницу
-function showTime() {
-  const targetDate2 = new Date();
-  const time = targetDate1 - targetDate2;
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  refs.days.textContent = days;
-  refs.hours.textContent = hours;
-  refs.mins.textContent = mins;
-  refs.secs.textContent = secs;
-}
+// Создаем экземпляр класса
+const oneCountdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date(2021, 0),
+});
 
-// Вызов функции showTime каждую секунду
-setInterval(showTime, 1000);
+// Вызываем метод для перевоначальной инициализации элементов таймера
+oneCountdownTimer.showTime();
+// Запускаем таймер
+oneCountdownTimer.tickTime();
